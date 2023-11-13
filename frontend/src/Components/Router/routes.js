2890d12698887fa848a2d38e17364f1f3aa292a1
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2023-11-13 14:49:19 +0100

    add: routes 2 games

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,15 @@
 import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
 import NewPage from '../Pages/NewPage';
+import AlgoGamePage from '../Pages/AlgoGamePage';
+import ModuloGamePage from '../Pages/ModuloGamePage';
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
   '/new': NewPage,
+  '/game/algo': AlgoGamePage,
+  '/game/modulo': ModuloGamePage
 };
 
 export default routes;

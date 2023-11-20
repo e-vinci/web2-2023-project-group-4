d37d284d0424375c8539e9a6a592f827983ff2da
oceane-tran-vinci <thuyduong.tran@student.vinci.be>
2023-11-20 15:38:06 +0100

    add: Register and Login Pages (issues #17)

--- frontend/src/Components/Router/routes.js
+++ frontend/src/Components/Router/routes.js
@@ -1,11 +1,16 @@
-import GamePage from '../Pages/GamePage';
 import HomePage from '../Pages/HomePage';
-import NewPage from '../Pages/NewPage';
+import GamePage from '../Pages/GamePage';
+import LoginPage from '../Pages/LoginPage';
+import RegisterPage from '../Pages/RegisterPage';
+import Logout from '../Logout/Logout';
+
 
 const routes = {
   '/': HomePage,
   '/game': GamePage,
-  '/new': NewPage,
+  '/login': LoginPage,
+  '/register': RegisterPage,
+  '/logout': Logout,
 };
 
 export default routes;
